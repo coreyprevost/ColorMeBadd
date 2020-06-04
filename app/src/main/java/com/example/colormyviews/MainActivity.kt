75ac406ec71b89d4f7634cad33d524d89743f06e
“Corey <60476487+coreyprevost@users.noreply.github.com>
2020-06-03 23:07:44 -0500

    Random Colors
    Added ability for random colors when boxes are clicked

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -3,20 +3,22 @@ package com.example.colormyviews
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.view.View
-import android.widget.ListView
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //makes function in tbe main class
         setListeners()
-    }
-
-    private fun setListeners() {
+        val redButton = findViewById<Button>(R.id.btn_Red)
+        val greenButton = findViewById<Button>(R.id.btn_Green)
+        val yellowButton = findViewById<Button>(R.id.btn_Yellow)
         val clickableViews: List<View> =
             listOf(
                 box_one_text,
@@ -26,30 +28,49 @@ class MainActivity : AppCompatActivity() {
                 tvboxfive,
                 btn_Red,
                 btn_Green,
-                btn_Yellow
+                btn_Yellow,
+                constraint_layout
             )
+        val buttonArray = arrayOf(btn_Green, btn_Yellow, btn_Red)
+        redButton.setOnClickListener {
+            for (items in clickableViews.minus(buttonArray)) {
+                items.setBackgroundColor(Color.RED)
+            }
+        }
+        greenButton.setOnClickListener {
+            for (items in clickableViews.minus(buttonArray)) {
+                items.setBackgroundColor(Color.GREEN)
+            }
+        }
+        yellowButton.setOnClickListener {
+            for (items in clickableViews.minus(buttonArray)) {
+                items.setBackgroundColor(Color.YELLOW)
+            }
+        }
+    }
 
+
+    //creates unction which creates value which makes list clickable
+    public fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(
+                box_one_text,
+                tv_box_two,
+                tv_boxthree,
+                tv_boxfour,
+                tvboxfive,
+                btn_Red,
+                btn_Green,
+                btn_Yellow,
+                constraint_layout
+            )
+        //sets on click listeners for all views to use make colored function
         for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
+            item.setOnClickListener { item.setBackgroundColor(Random.nextInt()) }
         }
+
     }
-    private
-    fun makeColored(view: View){
-        when (view.id){
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.tv_box_two -> view.setBackgroundColor(Color.GRAY)
-            R.id.tv_boxthree -> view.setBackgroundResource(android.R.color.holo_green_light)
-            R.id.tv_boxfour -> view.setBackgroundResource(android.R.color.background_dark)
-            R.id.tvboxfive -> view.setBackgroundResource(android.R.color.holo_green_light)
 
 
-
-            // Boxes using custom colors for background
-            R.id.btn_Red -> tv_boxthree.setBackgroundResource(R.color.clr_Red)
-            R.id.btn_Yellow -> tv_boxfour.setBackgroundResource(R.color.clr_Yellow)
-            R.id.btn_Green -> tvboxfive.setBackgroundResource(R.color.clr_Green)
-
-            else -> view.setBackgroundColor(Color.LTGRAY)
-        }
-    }
 }
+
